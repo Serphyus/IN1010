d93bfafc183aa6c66fa4e25f12ed3f17a1276160
Sigurd-Pettersen <73425444+Sigurd-Pettersen@users.noreply.github.com>
2022-03-15 14:41:06 +0100

    fixed wrong type in Vanlig class

--- Vanlig.java
+++ Vanlig.java
@@ -2,7 +2,7 @@ public class Vanlig extends Legemiddel {
     // Arver all Legemiddel funksjonalitet uten endring for
     // å kunne lage objekter siden Legemiddel er abstrakt
 
-    public Vanlig(String navn, int pris, float virkestoff){
+    public Vanlig(String navn, int pris, double virkestoff){
         super(navn, pris, virkestoff);
     }
 }
